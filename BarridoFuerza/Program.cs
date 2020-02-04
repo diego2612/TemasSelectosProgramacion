2306d1f6b2090f7ef691034ca54508c7abbc9ca2
Issac <issacatedra@gmail.com>
2020-02-04 16:37:41 -0600

    Ejercicio de clase terminado. Muestra Fuerza resultante, suma de fuerzas y se detiene cuando la carga puntual presenta F=0

--- BarridoFuerza/Program.cs
+++ BarridoFuerza/Program.cs
@@ -12,17 +12,27 @@ namespace BarridoFuerza
         const long K = 9000000000;
         static void Main()
         {
-            /* Console.Write("Para las cargas se tiene una distancia:,ingresa un espacio");
-             Console.ReadLine();*/
-            for (double i = 0.001; i < d; i += 0.001)
-            {
-                double Fuerza13 = Fuerza(q1, q2, i);
-                double Fuerza23 = Fuerza(q2,q3,d-i);
-                Console.WriteLine("Fuerza3 debido a 1 = {0} [N],  Fuerza 3 debido a 2 {1}    En una distancia de {2}",Fuerza13,Fuerza23, i);
-            }
+            Console.WriteLine("Este programa te permite ubicar el punto en que una carga presenta F = 0\t");
+            Console.WriteLine("Presiona enter");
+            Console.ReadLine();
 
-            double f = Fuerza(q1, q2, d);
-                Console.WriteLine(f);
+            for (double i = 0.01; i < d; i += 0.01)
+            {
+                double Fuerza13 = Fuerza(q1, q3, i);
+                double Fuerza23 = Fuerza(q2,q3,d-i);
+                double Fresultante = Fuerza13 - Fuerza23;
+                //Con esta condicional se sabe que q3 ha sobrepasado el punto en que su fuerza resultante es negativa 
+                if (Fresultante < 0)
+                {
+                    break;
+                }
+                Console.WriteLine("Fuerza en q3 debido a q1 = {0} [N],  Fuerza en q3 debido a q2 {1} [N]",Fuerza13,Fuerza23);
+                
+                Console.WriteLine("La fuerza resultantes es: {0} [N]", Fresultante);
+                Console.WriteLine(" En una distancia de {0}\n", i);
+                
+                
+            }
             Console.ReadLine();
         }
 
