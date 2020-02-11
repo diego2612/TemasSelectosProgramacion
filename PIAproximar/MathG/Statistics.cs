a7a118085a380b2ba46e661d02b9e1458362b7e3
Issac <issacatedra@gmail.com>
2020-02-11 15:29:03 -0600

    Introduccion de libreria estandar con metodos matematicos(suma)

---
+++ PIAproximar/MathG/Statistics.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MathG
+{
+    public class Statistics
+    {
+        public static double Sum(double[] entrada)
+        {
+            double salida = 0;
+            for (int i = 0; i < entrada.Length; i++)
+            {
+                salida = salida + entrada[i];
+            }
+            return 0;
+        }
+
+    }
+}
---
+++ PIAproximar/Sumatorias/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathG;
+
+namespace Sumatorias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t~~~~~~~~~~~~FACULTAD DE INGENIERIA~~~~~~~~~~~~");
+            Console.WriteLine("\t\t\tSanchez Catedra Diego Issac\n\t\t\tTemas Selectos de Programacion\n");
+            Console.WriteLine("\t\t\tSUMATORIAS");
+
+
+            double[] arreglo = new double[5] { 1,2,3,4,5};
+            double s = Statistics.Sum(arreglo);
+            double prom = s / args.Length;
+            Console.WriteLine("{ 0}", prom);
+            Console.ReadLine();
+            double suma = Statistics.Sum(arreglo);
+
+        }
+
+        static double Suma(double[] input)
+        {
+            //Metodo para sumar los arreglos 
+            double SumaArreglos = Statistics.Sum(input);
+            return SumaArreglos;
+        }
+
+        /*
+        static double Sum(double[] entrada)
+        {
+            double salida = 0;
+            for (int i = 0; i < entrada.Length; i++)
+            {
+                 salida = salida + entrada[i];
+            }
+            return 0;
+        }
+        */
+    }
+}
